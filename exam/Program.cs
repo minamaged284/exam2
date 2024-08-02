29b60454edd723c5a8c703e57357caa6a597a960
minamaged284 <minamina284@gmail.com>
2024-08-02 23:23:11 +0300

    final update

--- exam/Program.cs
+++ exam/Program.cs
@@ -16,13 +16,10 @@ namespace exam
 
 
 
-            //Subject subject = new Subject(1,"computer science");
-            //subject.setupExam();
+            Subject subject = new Subject(1, "computer science");
+            subject.setupExam();
 
-            Answer[] answers = new Answer[] { new Answer(1, "2"), new Answer(1, "2"), }
 
-            Answer answer1 = new Answer(1, "2");
-            Console.WriteLine();
 
 
 
